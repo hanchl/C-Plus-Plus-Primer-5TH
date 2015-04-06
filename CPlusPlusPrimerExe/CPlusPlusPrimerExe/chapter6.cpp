a9ddcf5031c53e503fb5ca1e21d2df222114d6a5
hanchl <hanchl@live.com>
2015-04-06 22:14:09 +0800

    add 6_36

--- CPlusPlusPrimerExe/CPlusPlusPrimerExe/chapter6.cpp
+++ CPlusPlusPrimerExe/CPlusPlusPrimerExe/chapter6.cpp
@@ -41,3 +41,10 @@ void Exe6_33(vector<int> v)
 	v.pop_back();
 	Exe6_33(v);
 }
+
+int odd[] = { 1, 3, 5, 7, 9 };
+int even[] = { 0, 2, 4, 6, 8 };
+auto Exe6_38(int i)->int(&)[5]
+{
+	return i & 2 == 1 ? odd : even;
+}
