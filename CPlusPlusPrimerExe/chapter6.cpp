b449ac8c19fb4854b4b84681915ae0220b44b05c
hanchl <hanchl@live.com>
2015-05-10 16:08:14 +0800

    delete useless files

---
+++ CPlusPlusPrimerExe/chapter6.cpp
@@ -0,0 +1,51 @@
+#include"CplusPlusExe.h"
+
+int Exe6_27(initializer_list<int> il)  //6.27 ·µ»Øinitializer_list<int> µÄºÍ
+{
+	if (il.size() <= 0)
+		exit(1);
+	int sum = 0;
+	for (const auto &i : il)
+		sum += i;
+	return sum;
+};
+//Exe6_28 the type of elem :const string &;
+//Exe_6_29 use an reference can reduce the cost of memory
+
+bool Exe6_30(const string &str1, const string &str2)
+{
+	if (str1.size() == str2.size())
+	{
+		return str1 == str2;
+	}
+	auto size = str1.size() < str2.size() ? str1.size() : str2.size(); {
+		for (decltype(size) i = 0; i != size; ++i)
+			if (str1[i] != str2[i])
+				return   false;  
+	}
+	return false;
+}
+
+
+
+int &Exe6_32(int *array, int index)
+{
+	return array[index];
+}
+
+void Exe6_33(vector<int> v)
+{
+	if (v.size() == 0)
+		return;
+	cout << v.back() << endl;
+	v.pop_back();
+	Exe6_33(v);
+}
+
+int odd[] = { 1, 3, 5, 7, 9 };
+int even[] = { 0, 2, 4, 6, 8 };
+auto Exe6_38(int i)->int(&)[5]
+{
+	return i % 2 == 1 ? odd : even;
+}
+
---
+++ CPlusPlusPrimerExe/main.cpp
@@ -0,0 +1,52 @@
+#include"CplusPlusExe.h"
+//main()函数，只做调用
+//Exe6_51
+
+void f()
+{
+	cout << " f with null parameters\n";
+}
+
+void f(int ival)
+{
+	cout << "f with one int parameter:" << ival << endl;
+}
+
+void f(int ival1, int ival2)
+{
+	cout << "f with two int parameters :" << ival1 << "  " << ival2 << endl;
+}
+
+void f(double dval1, double dval2 = 3.14)
+{
+	cout << "f with two double parameters :" << dval1 << "   " << dval2 << endl;
+}
+
+//test for recusion loop 
+int recloop(int val)
+{
+	if (val > 1)
+		return recloop(val-1)*val;
+	return 1;
+};   
+
+int array[10];
+
+int( &func() ) [10]
+{
+	return  array;
+}
+
+int main()
+{
+	//cout << Exe6_27({1});
+	//Exe5_21();
+	
+	/*vector<int> v = { 1, 2, 3, 4, 5 };
+	Exe6_33(v);*/
+	f(2.56, 3.14);
+
+	system(" pause ");
+
+	return 0;
+}
