db45ec7f52d904fdb3cc776fb3a0bbeb8d506199
C-Plus-Plus-Primer-5TH <hanchl@live.com>
2015-05-11 12:48:18 +0800

    add file chapter8.cpp

---
+++ CPlusPlusPrimerExe/chapter8.cpp
@@ -0,0 +1,86 @@
+#include "CplusPlusExe.h"
+#include <fstream>
+#include <sstream>
+#include <string>
+
+istream&  Exe8_1(istream& in)
+{
+	auto old_state = in.rdstate();
+	in.clear();
+	
+	while(!in.eof())
+	{
+		string buff;
+		in>>buff;
+		cout<<buff<<endl;  
+	}
+
+	in.setstate(old_state);
+///for test
+	// if (in.eof())
+	// {
+	// 	cout<<"cin eof \n";
+	// }
+	// if (in.fail())
+	// {
+	// 	cout<<"cin fail \n";
+	// }
+	// if (in.bad())
+	// {
+	// 	cout<<"cn bad \n";
+	// }
+
+	// cout<<in.rdstate()<<endl;
+	// cout<<old_state<<endl;
+	// cout<<in.rdstate()<<endl;
+	
+	// in.clear(in.rdstate() | old_state);
+	// cout<<in.rdstate()<<endl;
+
+	return in;
+}
+
+void Exe8_4()
+{
+	ifstream infile("../CMakeLists.txt");
+	if (!infile.is_open())
+	{
+		cerr<<"could not open CMakeLists.txt";
+		exit(1);
+	}
+	else
+	{
+		vector<string> CMakeLists;
+		string line;
+		while(getline(infile,line))
+		{
+			CMakeLists.push_back(line);
+		}
+		for (const auto &i : CMakeLists)
+		{
+			cout<<i<<endl;
+		}
+	}
+}
+void Exe8_5()
+{
+	ifstream infile("../CMakeLists.txt");
+	if (!infile.is_open())
+	{
+		cerr<<"could not open CMakeLists.txt";
+		exit(1);
+	}
+	else
+	{
+		vector<string> CMakeLists;
+		string word;
+		while(infile>>word)
+		{
+			CMakeLists.push_back(word);
+		}
+		for (const auto &i : CMakeLists)
+		{
+			cout<<i<<endl;
+		}
+	}
+}
